639f631d62176d5815508a7799a871a61cce7096
1n1tial <dan.yun0821@gmail.com>
2023-11-30 15:08:30 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.techwithtim.net/tutorials/python-online-game-tutorial/online-rock-paper-scissors-p-4
--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import pygame
+from network import Network
+import pickle
 
 pygame.init()
 
@@ -21,7 +23,32 @@ pygame.display.set_caption('Super Tic Tac Toe')
 
 player_turn = 'X'  # Player X always goes first
 
-def draw_board(screen, main_board, small_boards):
+
+class Button:
+    def __init__(self, text, x, y, color):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = 150
+        self.height = 100
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont(None, 40)
+        text = font.render(self.text, 1, (255,255,255))
+        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
+
+    def click(self, pos):
+        x1 = pos[0]
+        y1 = pos[1]
+        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            return True
+        else:
+            return False
+        
+
+def draw_board(screen, main_board, small_boards, game):
     screen.fill(GRID_COLOR)  # Fill the screen with the grid color
 
     # Draw the main board grid
@@ -29,6 +56,8 @@ def draw_board(screen, main_board, small_boards):
         pygame.draw.line(screen, CELL_COLOR, (i * CELL_SIZE*3, 0), (i * CELL_SIZE*3, WINDOW_SIZE[1]), 5)
         pygame.draw.line(screen, CELL_COLOR, (0, i * CELL_SIZE*3), (WINDOW_SIZE[0], i * CELL_SIZE*3), 5)
 
+    
+    
     # Draw the smaller Tic Tac Toe boards within the main board
     for row in range(3):
         for col in range(3):
@@ -58,7 +87,6 @@ def draw_board(screen, main_board, small_boards):
                                                 row * CELL_SIZE*3 + CELL_SIZE*3 // 2))
                 screen.blit(text, text_rect)
     
-    pygame.display.flip()
 
 def handle_click(pos):
     global player_turn
@@ -109,10 +137,8 @@ def check_win(board):
                 
     return ' '
 
+
 def display_outcome(screen, outcome):
-    # Display a message when a player wins or when the game ends in a draw
-    # Use pygame.font.Font() and render text to display the outcome message
-    
     if outcome == 'D':
         text = big_font.render('Draw!', True, OUT_COLOR)
     else:
@@ -120,28 +146,66 @@ def display_outcome(screen, outcome):
     text_rect = text.get_rect(center=(WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2))
     screen.blit(text, text_rect)
 
-running = True
-playing = True
-while running:
-    while playing:
+
+def main():
+    running = True
+    end = False
+    clock = pygame.time.Clock()
+    n = Network()
+    player = int(n.getP())
+    print("You are player", player)
+    
+    while running:
+        clock.tick(60)
+        try:
+            game = n.send("get")
+        except:
+            running = False
+            print("Couldn't get game")
+            break
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                playing = False
                 running = False
+                pygame.quit()
+                
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
                 if handle_click(mouse_pos):
-                    playing = False
+                    end = True
     
         draw_board(window, main_board, small_boards)
         
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
-    
-    if check_win(main_board) != ' ': display_outcome(window, check_win(main_board))
-    pygame.display.flip()
+        if end:
+            display_outcome(window, check_win(main_board))
+        
+        pygame.display.flip()
+        
+
+    pygame.quit()
     
 
-# Quit Pygame
-pygame.quit()
+def menu_screen():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        window.fill((128, 128, 128))
+        font = pygame.font.SysFont(None, 60)
+        text = font.render("Click to Play!", 1, (255,0,0))
+        window.blit(text, (100,200))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+
+    main()
+
+
+while True:
+    menu_screen()
